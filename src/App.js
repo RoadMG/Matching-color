e798de967df7d4bd04688415576be3d1631e5b5c
Mingyu Kil <90road@gmail.com>
2022-10-10 02:24:38 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+const getRandomColor = () => {
+  const digits = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "F",
+  ];
+  const number = new Array(6)
+    .fill("")
+    .map(() => digits[Math.floor(Math.random() * digits.length)])
+    .join("");
+  return `#${number}`;
+};
 
 function App() {
+  const [color, setColor] = useState("");
+  const [answers, setAnswers] = useState([]);
+  const [answerIndicator, setAnswerIndicator] = useState(undefined);
+
+  const generateColor = () => {
+    const answerColor = getRandomColor();
+    setColor(answerColor);
+    setAnswers(
+      [answerColor, getRandomColor(), getRandomColor()].sort(
+        () => Math.random() - 0.5
+      )
+    );
+  };
+  useEffect(() => {
+    generateColor();
+  }, []);
+
+  const chooseAnswer = (answer) => {
+    if (answer === color) {
+      setAnswerIndicator(false);
+      generateColor();
+    } else {
+      setAnswerIndicator(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <div className="box" style={{ backgroundColor: color }}></div>
+      <section>
+        {answers.map((answer) => (
+          <button onClick={() => chooseAnswer(answer)} key={answer}>
+            {answer}
+          </button>
+        ))}
+        {answerIndicator === true && <p className="wrong">Wrong Answer!!</p>}
+        {answerIndicator === false && <p className="correct">Correct!!</p>}
+      </section>
     </div>
   );
 }
